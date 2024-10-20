f723c4fd0321db2ffda25fece57401250029f59c
fadli25 <zakaria2.2fadli@gmail.com>
2024-10-20 20:20:10 +0100

    changes

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS Authors (
 );
 
 -- Create the books table
-CREATE TABLE IF NOT EXISTS books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(130) NOT NULL,
     author_id INT,
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS books (
 );
 
 -- Create the customers table
-CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS customers (
 );
 
 -- Create the orders table
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_id INT,
     order_date DATE NOT NULL,
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS orders (
 );
 
 -- Create the order_details table--
-CREATE TABLE IF NOT EXISTS order_details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
     book_id INT,
